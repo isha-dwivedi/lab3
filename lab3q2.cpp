d34026caead2da49f3d3f3bcf6657ce98d532c0d
isha-dwivedi <42205291+isha-dwivedi@users.noreply.github.com>
2018-08-17 02:50:27 -0400

    Add files via upload

---
+++ lab3q2.cpp
@@ -0,0 +1,39 @@
+//declare the library
+#include<iostream>
+using namespace std;
+//start the function
+int main()
+{
+//declare integer variables and simultaneously allot values
+int a=97, b=32;
+//declare operating variables and assign operations to them 
+int sum1=a+b;
+int sub1=a-b;
+int div1=a/b, mult1=a*b, mod1=a%b;
+//display the values of various operators
+cout<<"a="<<a<<endl<<"b="<<b<<endl;
+cout<<"sum="<<sum1<<endl;
+cout<<"subtraction="<<sub1<<endl;
+cout<<"multiplication="<<mult1<<endl;
+cout<<"division="<<div1<<endl;
+cout<<"module="<<mod1<<endl;
+
+//declare float variables and simultaneously allot values
+float p=12.4, q=21.4;
+//declare operating variables and assign operations to them 
+float sum2=p+q;
+float sub2=q-p;
+float div2=q/p, mult2=p*q;
+//display the values of various operators
+cout<<"p="<<p<<endl<<"q="<<q<<endl;
+cout<<"sum="<<sum2<<endl;
+cout<<"subtraction="<<sub2<<endl;
+cout<<"multiplication="<<mult2<<endl;
+cout<<"division="<<div2<<endl;
+
+
+
+}
+
+
+
