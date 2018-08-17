58367325979b6d6f284f74b9d62bdfab7f3f3f09
isha-dwivedi <42205291+isha-dwivedi@users.noreply.github.com>
2018-08-17 02:12:37 -0400

    Add files via upload

---
+++ lab3.cpp
@@ -0,0 +1,24 @@
+//include library
+#include<iostream>
+using namespace std;
+int main(){
+//declare variables and asiign value to them
+int i=12;
+float f=19.5;
+char c[5]="yay";
+bool b=false;
+double d=2.314354;
+cout<<"i="<<i; //display value of variables
+cout<<"size of int'i' is"<<sizeof(i)<<endl; //display size of variables
+cout<<"f="<<f;
+cout<<"size of float'f' is"<<sizeof(f)<<endl;
+cout<<"c="<<c;
+cout<<"size of char'c' is"<<sizeof(c)<<endl;
+cout<<"b="<<b;
+cout<<"size of bool'b' is"<<sizeof(b)<<endl;
+cout<<"d="<<d;
+cout<<"size of double'd' is"<<sizeof(d)<<endl;
+}
+
+//display the size of all variables
+
